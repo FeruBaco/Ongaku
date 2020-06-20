b319a2fed7214a38932490f4261955d3bbba4979
Luis Fernando Barajas Cortes <f1nandocor@gmail.com>
2020-06-19 19:42:08 -0500

    Create layout list

---
+++ app/src/main/java/com/example/ongaku/utilities/Utilities.java
@@ -0,0 +1,19 @@
+package com.example.ongaku.utilities;
+
+public class Utilities {
+    public static final String LIST_TABLE = "list";
+    public static final String SONG_TABLE = "song";
+    //LIST COLUMN NAME
+    public static final String LIST_ID = "id";
+    public static final String LIST_NAME = "name";
+    public static final String LIST_DATE = "date";
+    public static final String LIST_PHOTO = "photo";
+    //LIST COLUMN NAME
+    public static final String SONG_ID = "id";
+    public static final String SONG_NAME = "name";
+    public static final String SONG_ARTIST = "artist";
+    public static final String SONG_GENRE = "genre";
+    public static final String SONG_DATE = "date";
+    //CREATE TABLES
+    public static final String CREATE_LIST_TABLE = "CREATE TABLE "
+}
